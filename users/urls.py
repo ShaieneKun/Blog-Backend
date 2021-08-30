ad4fe3780ff6f253684e1fade894e7e16305dd10
ShaieneKun <rafaelshaiene115@gmail.com>
2021-08-30 01:23:24 -0300

    Using Django default login and logout views

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path 
 from . import views # Use this for functions
 from .views import CreateUser # Use this for classes
+from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = 'users'
 
 urlpatterns = [
-    path('create',CreateUser.as_view(), name='create'),
-    path('login',views.loginUser, name='login'),
+    path('create', CreateUser.as_view(), name='create'),
+    path('login', LoginView.as_view(template_name="users/login.html"), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
 ]
