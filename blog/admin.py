76d2d05700e0aa68a71abb73d8f662d1b6169d42
ShaieneKun <rafaelshaiene115@gmail.com>
2021-11-04 23:07:54 -0300

    Refactoring

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from .models import Article
+
 # Register your models here.
 admin.site.register(Article)
 
 # Changing site's displayed name
 
-admin.site.site_header = "Article Admin"
-admin.site.site_title = "Article Admin Area"
-admin.site.index_title = "Welcome to Article Admin Area"
-
+admin.site.site_header = "Blog admin area"
+admin.site.site_title = "Blog admin area"
+admin.site.index_title = "Welcome to the blog's admin area"
