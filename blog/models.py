0f5debba90f00cfd8dafd3b6ccfe766d60138471
ShaieneKun <rafalshaiene115@gmail.com>
2022-03-03 01:03:33 -0300

    Storing and showing summary of article on article list

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,8 @@ from ckeditor_uploader.fields import RichTextUploadingField
 class Article(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    summary = models.CharField(max_length=100, default="")
     body = RichTextUploadingField()
     tags = models.ManyToManyField("Tag")
 
