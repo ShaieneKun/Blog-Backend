737216b71cefc89c8fd93ceee523790068f5e572
ShaieneKun <rafaelshaiene115@gmail.com>
2022-08-11 22:41:15 -0300

    Added enviroment variables when not in debug mode

--- main/settings.py
+++ main/settings.py
@@ -171,13 +171,11 @@ CKEDITOR_UPLOAD_PATH = "uploads/"
 CKEDITOR_IMAGE_BACKEND = "pillow"
 CKEDITOR_BROWSE_SHOW_DIRS = True
 
-# TODO implement cloud storage only in PROD
-# Storages
+if not DEBUG:
+    # Storages
 
-#DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-# AWS_S3_SESSION_PROFILE
-
-#AWS_ACCESS_KEY_ID = ""
-#AWS_SECRET_ACCESS_KEY = ""
-#AWS_STORAGE_BUCKET_NAME = ""
+    AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+    AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+    AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
